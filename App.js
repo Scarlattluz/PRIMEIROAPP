746ecc202780de9ac69d78ebebb5874e3671cae0
Scarlatt Luz <contatoscarlatt@gmail.com>
2021-01-03 00:04:48 -0300

    adicionando style

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Image} from 'react-native';
 
 class App extends Component {
   render() {
+    let nome = 'Scar';
+    let img = 'https://sujeitoprogramador.com/steve.png';
+
     return (
       <View>
         <Text>Olá 2021!!!!</Text>
+        <Text>Primeiro aplicativo em React Native</Text>
+        <Text style={{color: '#FF0000', fontSize: 25, margin: 15}}>
+          Teste de estilo
+        </Text>
+
+        <Image source={{uri: img}} style={{width: 300, height: 300}} />
+
+        <Text style={{fontSize: 30}}>{nome}</Text>
       </View>
     );
   }
