a57cbf01cf36942fbef4246712aea7479f332310
Scarlatt Luz <contatoscarlatt@gmail.com>
2021-01-02 21:25:35 -0300

    🎉primeiro commit 2021🎉

---
+++ App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+
+class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Olá 2021!!!!</Text>
+      </View>
+    );
+  }
+}
+
+export default App;
